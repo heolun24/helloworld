b6050a8a5bcb469ece99b26828b3996fc121d3c6
heolun24 <quachquach987@gmail.com>
2021-02-05 10:41:44 +0700

    Add hello world

---
+++ App.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+
+import AssetExample from './components/AssetExample';
+
+
+import { Card } from 'react-native-paper';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+            Chao anh dat dep zai
+      </Text>
+   
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
